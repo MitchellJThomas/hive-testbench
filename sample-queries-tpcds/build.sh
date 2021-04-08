3e9a4de47e51a6b750e2ccaa94f2689614134a03
David W. Streever <david@streever.com>
2021-04-08 17:31:06 -0400

    .

--- sample-queries-tpcds/build.sh
+++ sample-queries-tpcds/build.sh
@@ -4,13 +4,12 @@ done
 
 Q_LEN=$(( ${#queryarr[@]}+1 ))
 
-echo "!record \${OUTPUT_FILE};"
 echo "use \${DB};"
 for (( i=0; i<$Q_LEN; i++ ))
 do
   echo "SELECT \"--marker--\", \"${queryarr[i]}\", \"${queryarr[i-1]}\",  unix_timestamp();"
   if [ "X${queryarr[i]}" != "X" ]; then
-    echo "!run ${queryarr[i]};"
+    echo "!run ${queryarr[i]}"
   fi
 done
 
--- sample-queries-tpcds/time.sh
+++ sample-queries-tpcds/time.sh
@@ -16,7 +16,7 @@ done
 RUN_FILE=`mktemp`
 echo "Temp RUN_FILE: ${RUN_FILE}"
 
-echo "!record ${OUTPUT};" > ${RUN_FILE}
+echo "!record ${OUTPUT}" > ${RUN_FILE}
 cat perf_run.sql >> ${RUN_FILE}
 
 hive --showHeaders=false --silent=true --outputformat=dsv  --hivevar DB=${DB} -f ${RUN_FILE}
