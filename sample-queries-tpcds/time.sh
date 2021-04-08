e2a20049b68f65ac944d78efee73f0db8ec6ceae
David W. Streever <david@streever.com>
2021-04-08 15:30:01 -0400

    .

--- sample-queries-tpcds/time.sh
+++ sample-queries-tpcds/time.sh
@@ -1,4 +1,4 @@
-hive --showHeaders=false --silent=true --outputformat=dsv  --hivevar DB=$1 --hivevar OUTPUT_FILE=$2 -e perf_run.sql
+hive --showHeaders=false --silent=true --outputformat=dsv  --hivevar DB=$1 --hivevar OUTPUT_FILE=$2 -f perf_run.sql
 
 
 
