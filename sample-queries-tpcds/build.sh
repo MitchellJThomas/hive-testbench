bda26ad4d8ccfaf48dd26eca4f90bade6e062201
David W. Streever <david@streever.com>
2021-04-08 15:24:23 -0400

    perf run

---
+++ sample-queries-tpcds/build.sh
@@ -0,0 +1,18 @@
+for i in q*.sql; do
+  queryarr=(${queryarr[@]} "${i}")
+done
+
+Q_LEN=$(( ${#queryarr[@]}+1 ))
+
+echo "!record \${OUTPUT_FILE}"
+
+for (( i=0; i<$Q_LEN; i++ ))
+do
+  echo "SELECT \"MARKER\", \"${queryarr[i]}\", \"${queryarr[i-1]}\",  unix_timestamp();"
+  if [ "X${queryarr[i]}" != "X" ]; then
+    echo "!run ${queryarr[i]};"
+  fi
+done
+
+echo "!record"
+
---
+++ sample-queries-tpcds/time.sh
@@ -0,0 +1,4 @@
+hive --showHeaders=false --silent=true --outputformat=dsv --hivevar OUTPUT_FILE=$1 -e perf_run.sql
+
+
+
