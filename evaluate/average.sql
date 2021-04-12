15d57e45b25734b08135ffb3e3cfd25ed081c056
David W. Streever <david@streever.com>
2021-04-12 11:13:51 -0400

    Added a detailed individual query times report.

--- evaluate/average.sql
+++ evaluate/average.sql
@@ -5,8 +5,8 @@ SELECT *
 FROM
     tpcds_queries
 where
-     start_query = 'query1.sql'
-  or end_query = 'query1.sql'
+     start_query = 'query60.sql'
+  or end_query = 'query60.sql'
 limit 10;
 
 -- TOTAL Time (average) across the queries for each dimension.
@@ -187,6 +187,91 @@ FROM
 ORDER BY
     query, avg_lapsed_seconds;
 
+-- Individual Query Results - Detailed
+WITH
+    CORRELATE AS (
+        SELECT
+            s.db,
+            s.start_query       as query,
+            s.marker            as began,
+            e.marker            as completed,
+            e.marker - s.marker as lapsed_seconds
+        FROM
+            tpcds_queries s
+                INNER JOIN tpcds_queries e
+                           on s.db = e.db
+                               and s.start_query = e.end_query
+                               and s.marker < e.marker
+        WHERE
+                s.start_query != ""
+    ),
+    RANKED_CORRELATED AS (
+        SELECT
+            db,
+            query,
+            began,
+            completed,
+            lapsed_seconds,
+            rank() over (partition by db, query, began order by lapsed_seconds asc) rank
+        FROM
+            CORRELATE
+    ),
+    MATCHED_QUERY AS (
+        SELECT
+            db,
+            query,
+            began,
+            completed,
+            lapsed_seconds
+        FROM
+            RANKED_CORRELATED
+        WHERE
+                rank = 1
+    ),
+    BREAKDOWN AS (
+        SELECT
+            --db,
+            case
+                WHEN size(split(db, "_")) = 6 THEN
+                    case
+                        when split(db, "_")[3] = "managed" then
+                            true
+                        else
+                            false
+                        end
+                ELSE
+                    case
+                        when split(db, "_")[4] = "managed" then
+                            true
+                        else
+                            false
+                        end
+                END                                  MANAGED,
+            case
+                WHEN size(split(db, "_")) = 6 THEN
+                    true
+                ELSE
+                    false
+                END                                  PARTITIONED,
+            split(db, "_")[size(split(db, "_")) - 2] FORMAT,
+            split(db, "_")[size(split(db, "_")) - 1] SCALE,
+            query,
+            lapsed_seconds lapsed_seconds
+        FROM
+            MATCHED_QUERY
+    )
+
+SELECT
+    QUERY,
+    lapsed_seconds,
+    MANAGED,
+    PARTITIONED,
+    FORMAT,
+    SCALE
+FROM
+    BREAKDOWN
+ORDER BY
+    query, lapsed_seconds;
 
 -- original
 select
